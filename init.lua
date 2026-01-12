fdff7466095993cc21d7ec9c158b7dafd778690a
ngtv <tuanvy860@gmail.com>
2026-01-12 17:56:29 +0700

    feat: remove _internal table and use _name for internal functions instead

--- init.lua
+++ init.lua
@@ -1,11 +1,10 @@
 -- init.lua
 local M = {}
-M._internal = {}
 
 local tsu = require("tree-sitter-utils")
 local match = require("match")
 
-M._internal.tsutils = tsu
-M._internal.match = match
+M._tsutils = tsu
+M._match = match
 
 return M
